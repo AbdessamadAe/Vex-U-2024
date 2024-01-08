46ea772069bb7e7582dbc9e5dba05c1f725e63d6
AbdessamadAe <ab.aitelmouden@gmail.com>
2024-01-08 11:53:27 +0100

    Drivetrain control

--- BigRobot/src/main.cpp
+++ BigRobot/src/main.cpp
@@ -8,6 +8,10 @@
 /*----------------------------------------------------------------------------*/
 
 // ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Drivetrain           drivetrain    1, 2, 3, 4
+// Controller1          controller
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
@@ -18,7 +22,6 @@ using namespace vex;
 competition Competition;
 
 // define your global instances of motors and other devices here
-
 /*---------------------------------------------------------------------------*/
 /*                          Pre-Autonomous Functions                         */
 /*                                                                           */
@@ -29,7 +32,8 @@ competition Competition;
 /*  not every time that the robot is disabled.                               */
 /*---------------------------------------------------------------------------*/
 
-void pre_auton(void) {
+void pre_auton(void)
+{
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
 
@@ -47,7 +51,8 @@ void pre_auton(void) {
 /*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
 
-void autonomous(void) {
+void autonomous(void)
+{
   // ..........................................................................
   // Insert autonomous user code here.
   // ..........................................................................
@@ -63,9 +68,11 @@ void autonomous(void) {
 /*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
 
-void usercontrol(void) {
+void usercontrol(void)
+{
   // User control code here, inside the loop
-  while (1) {
+  while (1)
+  {
     // This is the main execution loop for the user control program.
     // Each time through the loop your program should update motor + servo
     // values based on feedback from the joysticks.
@@ -75,6 +82,24 @@ void usercontrol(void) {
     // update your motors, etc.
     // ........................................................................
 
+    /*---------------------------------------------------------------------------*/
+    /*                              Drivetrain Control                            */
+    /*---------------------------------------------------------------------------*/
+    // Axis4 = Left/Right on the left joystick
+    // Axis2 = Up/Down on the right joystick
+    // controller1.Axis4.position() is for forward/backward movement
+    // The controller1.Axis4.position() is preceeded by a minus sign to reverse
+    // the direction of the right motors. This is so that the robot turns for e.g right when
+    // the joystick is pushed to right by rotating the righ wheels backwords and the
+    // left wheels forwards.
+
+    RightDriveSmart.spin(vex::directionType::fwd,
+                          Controller1.Axis2.position() - Controller1.Axis4.position(),
+                          vex::velocityUnits::pct);
+    LeftDriveSmart.spin(vex::directionType::fwd,
+                         Controller1.Axis2.position() + Controller1.Axis4.position(),
+                         vex::velocityUnits::pct);
+
     wait(20, msec); // Sleep the task for a short amount of time to
                     // prevent wasted resources.
   }
@@ -83,7 +108,8 @@ void usercontrol(void) {
 //
 // Main will set up the competition functions and callbacks.
 //
-int main() {
+int main()
+{
   // Set up callbacks for autonomous and driver control periods.
   Competition.autonomous(autonomous);
   Competition.drivercontrol(usercontrol);
@@ -92,7 +118,8 @@ int main() {
   pre_auton();
 
   // Prevent main from exiting with an infinite loop.
-  while (true) {
+  while (true)
+  {
     wait(100, msec);
   }
 }
